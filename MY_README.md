b8e1e8d44aa606c86aaa6e803b432aacb278681f
Joe Stowers <greenchiletexan@gmail.com>
2017-02-01 22:45:37 -0600

    Delete old src directory and create new one.  Initialize index.js

--- MY_README.md
+++ MY_README.md
@@ -1,1 +1,24 @@
-### February 1, 2017
+###February 1, 2017###
+
+index.html
+
+        <script src="/bundle.js"></script>
+
+        bundle.js is the compiled JavaScript for entire application.
+        Webpack and Babel put all the JavaScript files together in a single file.
+
+##Sec. 1, Lec. 6
+
+###What is React?###
+
+React is a JavaScript library used to produce HTML that is shown to user in a web browser.
+
+Writing individual components (views) => snippets of code that produce HTML.
+
+Nest components inside of others to create complex applications.
+
+###What is a Component?###
+
+A component is a collection of JavaScript functions that produce HTML.
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component.  
+// This component should produce some HTML.
 
-import App from './components/app';
-import reducers from './reducers';
+// const => ES6 syntax that declares a constant;
+// a constant will never change or reassign App down the line.
+const App = function() {
+	// JSX => dialect of JavaScript that allows us to 
+	// write what looks like HTML, but really is just
+	// JavaScript behind the scenes (thanks to Webpack and Babel)
+	return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM).
+
